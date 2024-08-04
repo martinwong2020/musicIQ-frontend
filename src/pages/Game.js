2f533fb7d595b882120933d88490417ced71a36b
Victor Lai <laivictor80@gmail.com>
2024-08-03 21:38:56 -0700

    udpated styling

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -116,58 +116,60 @@ function Game() {
   },[quizSong]);
   
   return (
-    <Container style={{ textAlign: 'center', marginTop: '50px' }}>
-      <div style={{ display: loading ? 'flex' : 'none', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <ReactLoading type="bars" color="orange" height={80} width={80} />
-      </div>
-      <Box sx={{display:searchedArtist? 'none':'block'}}>
-        <TextField id="outlined-basic" label="Artist" variant='outlined' onChange={(e)=>{setArtist(e.target.value)}}/>
-        <Button variant='contained' onClick={()=>{fetchSongs();}}>Search</Button>
-      </Box>
-      <Box sx={{display:insufficientSongs ? 'block' :'none'}}>
-        <h1>Sorry the following artist you have searched has less than 15 songs to be used for the game. Please try again with a new artist.</h1>
-      </Box>
-      {quizSong.length!=0 &&(
-        <Box>
-          <audio controls key={quizSong[songIndex].preview}>
-            <source src={quizSong[songIndex].preview} type="audio/mpeg"/>
-            
-          </audio>
-          <h1>{quizSong[songIndex]["title"]}</h1>
+      <Container style={{ textAlign: 'center', backgroundColor:"white", marginTop:"10%", padding:"20px", backgroundColor:"#161616"}}>
+        <div style={{ display: loading ? 'flex' : 'none', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <ReactLoading type="bars" color="orange" height={80} width={80} />
+        </div>
+        <Box sx={{display:searchedArtist? 'none':'flex', justifyContent:"center", alignItems:"center"}}>
+          <TextField id="outlined-basic" label="Artist" variant='filled' sx={{backgroundColor:"white",'& .MuiInputLabel-root.Mui-focused': 
+          {color: 'gray',fontSize:"20px"}}} onChange={(e)=>{setArtist(e.target.value)}}/>
+          <Button variant='contained' sx={{backgroundColor: '#4caf50',color: 'white','&:hover': {backgroundColor: '#388e3c',},marginLeft:"10px"}} onClick={()=>{fetchSongs();}}>Search</Button>
         </Box>
-      )}
-      
-      <Box width="80%" backgroundColor="black" alignItems="center">
-        {songs.length!=0 && quizSong.length!=0 && remaingingSong.length!=0 &&(
-          <Box display="flex" width="80%" alignItems="center">
-            {songChoices.map((song,index)=>(
-              <Card 
-                key={index} 
-                sx={{ 
-                  maxWidth: 345 , 
-                  border: (selectedChoice===index) ? correctChoice ? 'green 2px solid' : 'red 2px solid' : 'none'
-                }}>
-                <CardActionArea onClick={(e)=>{
-                  checkSongChoice(song["title"],index);
-                }}>
-                  <CardMedia 
-                    component="img"
-                    height="200"
-                    image={song["album"]["cover_big"]}
-                  />
-                  <CardContent sx={{backgroundColor:'#020202',color:'white'}}>
-                    <Typography gutterBottom variant="h5" component="div" >
-                      {song["title"]}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            ))}
+        <Box sx={{display:insufficientSongs ? 'block' :'none'}}>
+          <h1>Sorry the following artist you have searched has less than 15 songs to be used for the game. Please try again with a new artist.</h1>
+        </Box>
+        {quizSong.length!=0 &&(
+          <Box>
+            <audio controls key={quizSong[songIndex].preview}>
+              <source src={quizSong[songIndex].preview} type="audio/mpeg"/>
+              
+            </audio>
+            <h1>{quizSong[songIndex]["title"]}</h1>
           </Box>
-        )}    
-        <Button variant='contained'sx={{display: (selectedChoice!=null) ? 'block':'none'}} onClick={()=>{resetChoices()}}>Continue</Button>
-      </Box>
-    </Container>
+        )}
+        
+        <Box width="80%" backgroundColor="black" alignItems="center">
+          {songs.length!=0 && quizSong.length!=0 && remaingingSong.length!=0 &&(
+            <Box display="flex" width="80%" alignItems="center">
+              {songChoices.map((song,index)=>(
+                <Card 
+                  key={index} 
+                  sx={{ 
+                    maxWidth: 345 , 
+                    border: (selectedChoice===index) ? correctChoice ? 'green 2px solid' : 'red 2px solid' : 'none',
+                    
+                  }}>
+                  <CardActionArea onClick={(e)=>{
+                    checkSongChoice(song["title"],index);
+                  }}>
+                    <CardMedia 
+                      component="img"
+                      height="200"
+                      image={song["album"]["cover_big"]}
+                    />
+                    <CardContent sx={{backgroundColor:'#020202',color:'white'}}>
+                      <Typography gutterBottom variant="h5" component="div" >
+                        {song["title"]}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              ))}
+            </Box>
+          )}    
+          <Button variant='contained'sx={{display: (selectedChoice!=null) ? 'block':'none', backgroundColor: '#4caf50',color: 'white','&:hover': {backgroundColor: '#388e3c',},}} onClick={()=>{resetChoices()}}>Continue</Button>
+        </Box>
+      </Container>
   )
 }
 
